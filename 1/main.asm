d0a0c16ffd90b1f4b4fd45aa1dd6162acc065e63
kobyakov <dmi.kobyakov@gmail.com>
2022-01-02 00:51:27 +0300

    Simplify

--- 1/main.asm
+++ 1/main.asm
@@ -5,11 +5,12 @@ _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi,message
-	mov rdx, 13
+	mov rdx, len
 	syscall
 	mov rax, 60
 	xor rdi, rdi
 	syscall
 
 section .data
-message: db "test", 4
+message: db "test", 10
+len: equ $-message
